e6ed5d5a44c8fe3cf22c1c67c613370597e42e1f
owner <grrkekisbest@gmail.com>
2024-01-05 02:20:17 -0500

    Auto run ffplay works

--- run.sh
+++ run.sh
@@ -29,11 +29,16 @@ do
 done
 echo 'Pick a number: '
 read option
-echo $option
+echo ${x[$option-1]}
+
+ffplay -i ${x[$option-1]} -framerate 60 -video_size 640x360
+
+### above is all working
+
 
 r=wmctrl -l | grep -E -i '\/dev+\/\w+' | cut -d' ' -f1
 xwininfo -id $r
 
-ffplay -i /dev/video3 -framerate 60 -video_size 640x360
+
 ffmpeg -f x11grab -video_size 640x360 -framerate 10 -i :0.0799,511 -vf format=yuv420p test.mp4
 
