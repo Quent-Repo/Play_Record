7bd59e89f338860f4bdb85c993e63dabf48a3a17
owner <grrkekisbest@gmail.com>
2024-01-05 01:45:55 -0500

    added more

--- run.sh
+++ run.sh
@@ -1,7 +1,40 @@
 #/bin/sh
+#v4l2-ctl --list-devices
+
+##Gets the cams and puts them into
+f=1
+cams_names=$(v4l2-ctl --list-devices | grep -E -i '.+:$')
+cams_names_input=$(v4l2-ctl --list-devices | grep -E -i '/\w+/.+')
+IFS=$'\n' read -rd '' -a y <<<"$cams_names"
+cams_names_input_spliter=$'\n' read -rd '' -a x <<<"$cams_names_input"
+
+
+echo 'Do you want to use: 
+'
+for i in "${y[@]}"
+do
+	echo "$i"
+	for b in "${x[@]}"
+	do
+		if [ $f -eq 4 ]
+		then
+			((f=1))
+			echo $f. "$b"
+			((f++))
+		#if [ $f -lt 3 ]
+		else
+			echo $f. "$b"
+			((f++))
+		fi
+	done
+done
+echo 'Pick a number: '
+read option
+echo $option
+
+r=wmctrl -l | grep -E -i '\/dev+\/\w+' | cut -d' ' -f1
+xwininfo -id $r
+
 ffplay -i /dev/video3 -framerate 60 -video_size 640x360
 ffmpeg -f x11grab -video_size 640x360 -framerate 10 -i :0.0799,511 -vf format=yuv420p test.mp4
 
-v4l2-ctl --list-devices
-r=wmctrl -l | grep -E -i '\/\w+' | cut -d' ' -f1
-xwininfo -id $r
